cafc964b43fb35c2abdb6b291800899745bb61e6
Alina_Blazhko <alinablazhko420@gmail.com>
2018-08-31 14:13:11 +0300

    final changes for gists api

--- src/test/java/GistsTests.java
+++ src/test/java/GistsTests.java
@@ -1,54 +1,83 @@
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpStatus;
-import org.testng.Assert;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.expect;
 import static io.restassured.RestAssured.given;
 
 public class GistsTests {
     @Test
-    public void checkPutQuery(){
-        RestAssured.baseURI = "https://api.github.com/gists/e90edbb214cdd696f350e776eb681f2a?access_token=c196bd3fc948d302c5bca71090d880cdcda8d5cd";
-        RequestSpecification httpRequest = RestAssured.given();
-        Response response = httpRequest.get();
-
-        Integer statusCode = response.statusCode();
-        System.out.println(statusCode);
-
-        Assert.assertTrue(200 == statusCode);
-    }
-
-    @Test
-    public void postQuery(){
-        RestAssured.baseURI = "https://api.github.com/gists/public";
-        RequestSpecification httpRequest = RestAssured.given()
-                .contentType("application/json")
+    public void postQuery() {
+        given()
+                .auth().oauth2("a5e59c0252842d5457b2680e3077814f3676749f")
                 .body("{\n" +
                         "  \"description\": \"Hello World Examples\",\n" +
                         "  \"public\": true,\n" +
                         "  \"files\": {\n" +
                         "    \"hello_world.rb\": {\n" +
                         "      \"content\": \"class HelloWorld\\n   def initialize(name)\\n      @name = name.capitalize\\n   end\\n   def sayHi\\n      puts \\\"Hello !\\\"\\n   end\\nend\\n\\nhello = HelloWorld.new(\\\"World\\\")\\nhello.sayHi\"\n" +
-                        "   }}}");
-
-        Response response = httpRequest.
-                post();
-        System.out.println(response.statusCode());
-//        RestAssured.baseURI  = "https://api.github.com/gists/e90edbb214cdd696f350e776eb681f2a?access_token=c196bd3fc948d302c5bca71090d880cdcda8d5cd";
-//
-//        Response r = given()
-//                .contentType("application/json").
-//                        body("{\"description\": \"Hello World Examples\",\"public\": true,\"hello_world.py\":{\"content\": \"class HelloWorld:\\n\\n\"}}").
-//                        when().
-//                        post();
-//
-//        String body = r.getBody().asString();
-//        System.out.println(body);
-
-
+                        "    },\n" +
+                        "    \"hello_world.py\": {\n" +
+                        "      \"content\": \"class HelloWorld:\\n\\n    def __init__(self, name):\\n        self.name = name.capitalize()\\n       \\n    def sayHi(self):\\n        print \\\"Hello \\\" + self.name + \\\"!\\\"\\n\\nhello = HelloWorld(\\\"world\\\")\\nhello.sayHi()\"\n" +
+                        "    },\n" +
+                        "    \"hello_world_ruby.txt\": {\n" +
+                        "      \"content\": \"Run `ruby hello_world.rb` to print Hello World\"\n" +
+                        "    },\n" +
+                        "    \"hello_world_python.txt\": {\n" +
+                        "      \"content\": \"Run `python hello_world.py` to print Hello World\"\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "}")
+                .post("https://api.github.com/gists")
+                .then().assertThat().statusCode(201);
     }
+
+    @Test
+    public void putQuery(){
+        // Find id for our gist
+        JsonPath id = given().auth().oauth2("a5e59c0252842d5457b2680e3077814f3676749f").get("https://api.github.com/gists").getBody().jsonPath();
+
+        given().auth().oauth2("a5e59c0252842d5457b2680e3077814f3676749f")
+                .when()
+                .put("https://api.github.com/gists/" + id.getList("id").get(0) + "/star")
+                .then()
+                .assertThat()
+                .statusLine("HTTP/1.1 204 No Content");
+    }
+
+    @Test
+    public void patchQuery() {
+        // Find id for our gist
+        JsonPath id = given().auth().oauth2("a5e59c0252842d5457b2680e3077814f3676749f").get("https://api.github.com/gists").getBody().jsonPath();
+
+        given()
+                .auth().oauth2("a5e59c0252842d5457b2680e3077814f3676749f")
+                .body("{\n" +
+                        "  \"description\": \"Hello World Examples\",\n" +
+                        "  \"files\": {\n" +
+                        "    \"hello_world_ruby.txt\": {\n" +
+                        "      \"content\": \"Run `ruby hello_world.rb` or `python hello_world.py` to print Hello World\",\n" +
+                        "      \"filename\": \"hello_world.md\"\n" +
+                        "    },\n" +
+                        "    \"hello_world_python.txt\": null,\n" +
+                        "    \"new_file.txt\": {\n" +
+                        "      \"content\": \"This is a new placeholder file.\"\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "}")
+                .patch("https://api.github.com/gists/" + id.getList("id").get(0))
+                .then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void deleteQuery() {
+        // Find id for our gist
+        JsonPath id = given().auth().oauth2("a5e59c0252842d5457b2680e3077814f3676749f").get("https://api.github.com/gists").getBody().jsonPath();
+
+        System.out.println("Session id: " + id.getList("id").get(0));
+        given()
+                .auth().oauth2("a5e59c0252842d5457b2680e3077814f3676749f")
+                .delete("https://api.github.com/gists/" + id.getList("id").get(0))
+                .then().assertThat().statusCode(204);
+    }
+
 }
